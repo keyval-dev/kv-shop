e02ab8192b5fd64c20f2ae8de28438f75985de84
Eden Federman <eden@keyval.dev>
2023-09-25 13:38:50 +0300

    KV shop

---
+++ coupon/app.js
@@ -0,0 +1,47 @@
+const express = require('express');
+
+const PORT = parseInt(process.env.PORT || '8080');
+const app = express();
+const axios = require('axios');
+
+function getRandomNumber(min, max) {
+  return Math.floor(Math.random() * (max - min) + min);
+}
+
+app.get('/coupons', (req, res) => {
+  console.log('Coupon request received!');
+
+  // Fetch from membership api
+  axios.get(`http://${process.env.MEMBERSHIP_SERVICE_URL}/isMember`)
+    .then(function (response) {
+      console.log('Got response from membership service!', response.data);
+      res.json({
+        coupon: getRandomNumber(1, 25),
+        isMember: response.data.isMember
+      });
+    })
+    .catch(function (error) {
+      console.log(error);
+    });
+});
+
+app.post('/apply-coupon', (req, res) => {
+  console.log('Applying coupon!');
+
+    // Fetch from membership api
+    axios.get(`http://${process.env.MEMBERSHIP_SERVICE_URL}/isMember`)
+    .then(function (response) {
+      console.log('Got response from membership service!', response.data);
+      res.json({
+        coupon: getRandomNumber(1, 25),
+        isMember: response.data.isMember
+      });
+    })
+    .catch(function (error) {
+      console.log(error);
+    });
+});
+
+app.listen(PORT, () => {
+  console.log(`Listening for requests on port ${PORT}`);
+});
